e58be44817617faa17b6e4595ee8253a3f3d125a
Seth Rothstein <sethmrothstein@gmail.com>
2024-02-20 18:01:40 -0800

    Reorganized Explorations folder

--- Class/test.c
+++ Class/test.c
@@ -1,32 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <stddef.h>
+#include <stdalign.h>
 
-int main(void) {
-    printf("Hello!\n");
-    pid_t pid;
-
-    pid = fork();
-
-    if (pid == 0) {
-        // Child
-        // execlp("ls", "ls", "-l", NULL);
-        char *args[] = {
-            "ls",
-            "ls",
-            "-l",
-            NULL
-        };
-
-        execvp(args[0], args);
-        perror("exec");
-        exit(0);
-    } else {
-        // Parent
-        wait(NULL);
-    }
-    printf("Goodbye!\n");
+int main(void)
+{
+    printf("%zu\n", alignof(max_align_t));
 }
